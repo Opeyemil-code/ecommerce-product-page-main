352ed525444d7f4530b7ac6e5df02b87de40626f
Opeyemil-code <opeyemitoheeb31@gmail.com>
2022-10-22 11:28:13 -1200

    did modal window today and toggle button

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 'use strict';
+const mobileMenu = document.getElementById("mobilemenu");
+const orderNumber = document.querySelectorAll('.cartnumber')
+const cartMessages = document.querySelector('.cartmodal')
+const navMenu = document.querySelector('.navbar')
+const overlay = document.querySelector('.overlay')
 
+//for icons
+const substractItem = document.querySelector('.substract');
+const addItem = document.querySelector('.add')
+
+
+function Mobilemenu(params) {
+    navMenu.classList.toggle('hidden')
+    overlay.classList.toggle('hidden')
+}
+
+mobileMenu.addEventListener(
+    'click', Mobilemenu
+)
+
+overlay.addEventListener(
+    'click', Mobilemenu
+)

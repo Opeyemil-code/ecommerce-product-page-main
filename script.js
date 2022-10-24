ccca4415a06be35339d0e30455de5b97fb5dd190
Opeyemil-code <opeyemitoheeb31@gmail.com>
2022-10-24 01:52:37 -1200

    done for today

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 'use strict';
 const mobileMenu = document.getElementById("mobilemenu");
 const orderNumber = document.querySelectorAll('.cartnumber')
-const cartMessages = document.querySelector('.cartmodal')
-const navMenu = document.querySelector('.navbar')
-const overlay = document.querySelector('.overlay')
+const cartMessages = document.getElementById("cartmodal")
+const navMenu = document.getElementById("navbar")
+const overlay = document.getElementById("overlay")
+const cancelBtn = document.getElementById("cancel")
 
 //for icons
 const substractItem = document.querySelector('.substract');
 const addItem = document.querySelector('.add')
 
-
+//FUNCTIONALITY FOR MOBILE MENU
 function Mobilemenu(params) {
     navMenu.classList.toggle('hidden')
     overlay.classList.toggle('hidden')
@@ -22,3 +23,26 @@ mobileMenu.addEventListener(
 overlay.addEventListener(
     'click', Mobilemenu
 )
+
+cancelBtn.addEventListener(
+    'click', Mobilemenu
+)
+
+/*let count = 0
+
+function handler(params) {
+    for (let i = 0; i < orderNumber.length; i++) {
+        count++
+        document.querySelectorAll('.cartnumber')[i] = count
+        
+    }
+}
+
+
+addItem.addEventListener(
+    'click', handler
+)
+*/
+
+
+

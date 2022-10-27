4bdb2641246efce298b740d53dc67a872bfb2c9c
Opeyemil-code <opeyemitoheeb31@gmail.com>
2022-10-27 08:40:57 -1200

    pull

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
-'use strict';
+
+"use strict";
 const mobileMenu = document.getElementById("mobilemenu");
-const orderNumber = document.getElementsByClassName('cartnumber')
-const cartMessages = document.getElementById("cartmodal")
-const navMenu = document.getElementById("navbar")
-const overlay = document.getElementById("overlay")
-const cancelBtn = document.getElementById("cancel")
+const orderNumber = document.getElementsByClassName("cartnumber");
+const cartMessages = document.getElementById("cartmodal");
+const navMenu = document.getElementById("navbar");
+const overlay = document.getElementById("overlay");
+const cancelBtn = document.getElementById("cancel");
+const cartIcon = document.getElementById("cart");
 
 //for icons
-const substractItem = document.querySelector('.substract');
-const addItem = document.querySelector('.add')
+const substractItem = document.querySelector(".substract");
+const addItem = document.querySelector(".add");
 
 //FUNCTIONALITY FOR MOBILE MENU
 function Mobilemenu(params) {
-    navMenu.classList.toggle('hidden')
-    overlay.classList.toggle('hidden')
+  navMenu.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
 }
 
-mobileMenu.addEventListener(
-    'click', Mobilemenu
-)
-
-overlay.addEventListener(
-    'click', Mobilemenu
-)
-
-cancelBtn.addEventListener(
-    'click', Mobilemenu
-)
-
-
-
-
 function handler(params) {
+  let [a, b, c] = orderNumber;
+
+  a.innerHTML++;
+  b.innerHTML++;
+  c.innerHTML++;
+
+  console.log(typeof orderNumber)
+}
+
+// orderNumber[].innerHTML = 5;
+
+mobileMenu.addEventListener("click", Mobilemenu);
+
+overlay.addEventListener("click", Mobilemenu);
+
+cancelBtn.addEventListener("click", Mobilemenu);
+
+addItem.addEventListener("click", handler);
+
+
+
+
+/*function handler(params) {
     for (let i = 0; i < orderNumber.length; i++) {
         document.getElementsByClassName('cartnumber')[i].value++ 
         console.log(i)
     }
     
-}
+}*/
 
 addItem.addEventListener(
     'click', handler
 )
-
-console.log(document.getElementsByClassName('cartnumber'))

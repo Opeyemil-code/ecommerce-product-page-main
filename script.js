e0edebdce7a1dbcead3c4b0414b0f3ac89facf73
Opeyemil-code <opeyemitoheeb31@gmail.com>
2022-12-21 13:27:07 -1200

    .

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 
 "use strict";
 const mobileMenu = document.getElementById("mobilemenu");
-const orderNumber = document.getElementsByClassName("cartnumber");
-const cartMessages = document.getElementById("cartmodal");
 const navMenu = document.getElementById("navbar");
 const overlay = document.getElementById("overlay");
-const cancelBtn = document.getElementById("cancel");
-const cartIcon = document.getElementById("cart");
 const cartDiv = document.getElementById("cartdiv");
 const modal = document.getElementById("modal");
-const delBtn = document.getElementById("delete");
-const emptyCartMsg = document.getElementById("emptymessage");
+
+//BUTTONS FOR CART, CHECKOUT BUTTONS.
 const checkOutBtn = document.getElementById("checkout");
 const addToCart = document.getElementById("addtocart");
 const price = document.getElementById("price");
-const total = document.getElementById("total")
+const total = document.getElementById("total");
+
+//FOR CART MESSAGES AND HIDDEN DIVS AND NUMBERS
+const orderNumber = document.getElementsByClassName("cartnumber");
+const cartMessages = document.getElementById("cartmodal");
+const emptyCartMsg = document.getElementById("emptymessage");
 
 //for icons
 const substractItem = document.getElementById("substract");
 const addItem = document.getElementById("add");
+const cartIcon = document.getElementById("cart");
+const delBtn = document.getElementById("delete");
+const cancelBtn = document.getElementById("cancel");
 
 //FUNCTIONALITY FOR MOBILE MENU
 function Mobilemenu(params) {
@@ -27,8 +31,9 @@ function Mobilemenu(params) {
   cartMessages.classList.add('hidden')
 }
 
+console.log(orderNumber)
 
-//FUNCTIONALITY FOR ADDING COUNT
+//FUNCTIONALITY FOR ADDING COUNT, three spans having same class
 let count = 0
 function handlerAdd(params) {
   let [a, b, c] = orderNumber;
@@ -41,7 +46,7 @@ function handlerAdd(params) {
   
 }
 
-//FUNCTINAITY FOR SUBSTRCACTING COUNT
+//FUNCTINAITY FOR SUBSTRCACTING COUNT three element with same id
 function handlerSubstract(params) {
     let [a ,b ,c] = orderNumber
    count--
@@ -51,7 +56,7 @@ function handlerSubstract(params) {
    
 }
 
-console.log(orderNumber)
+
 
 function cart(params) {
     cartMessages.classList.toggle('hidden')
@@ -62,17 +67,21 @@ function cart(params) {
 
 function cartHandle(params) {
     if (count > 0) {
+       
+
         cartDiv.classList.remove('hidden');
         modal.classList.remove("hidden")
         emptyCartMsg.classList.add("hidden")
         checkOutBtn.classList.remove("hidden")
+        Number(total.innerHTML) = price.value * orderNumber;
+        //total.innerHTML = price.value * orderNumber;
       }
       else if (count < 0) {
         modal.classList.add('hidden')
         checkOutBtn.classList.add("hidden")
-
-
-      }
+        Number(total.innerHTML) = price.value * orderNumber;
+        //total.innerHTML = price.value * orderNumber;
+}
       
 }
 
@@ -116,3 +125,11 @@ addToCart.addEventListener('click', addCart)
 
 
 
+const imageBtn = document.getElementById('image-click')
+
+function overlayLgScreen(params) {
+     overlay.classList.remove('hidden');
+     
+}
+
+imageBtn.addEventListener('click', overlayLgScreen)
